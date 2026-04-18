11847ebe011519b3f214666260270c98fa08f717
Santiago Jorda <santiagojorda@gmail.com>
2026-04-18 14:27:00 -0300

    elimino golang

--- golang/src/common/middleware/factory.go
+++
@@ -1,9 +0,0 @@
-package middleware
-
-func CreateQueueMiddleware(queueName string, connectionSettings ConnSettings) (Middleware, error) {
-	return nil, nil
-}
-
-func CreateExchangeMiddleware(exchange string, keys []string, connectionSettings ConnSettings) (Middleware, error) {
-	return nil, nil
-}
